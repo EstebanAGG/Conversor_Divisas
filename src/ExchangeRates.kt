f0687a6c2e5d4ab0c45b939a09c4d48c035417c1
Esteban A. Giménez <estebanalfons.gimenez@murciaeduca.es>
2026-02-14 11:51:31 +0100

    Tabla de Tasas actualizada Encapsulación en DataClass de todas las listas de tasas Actualización de función actualizar tasas

--- src/ExchangeRates.kt
+++ src/ExchangeRates.kt
@@ -4,20 +4,40 @@
 enum class Divisas {
     Dolares, Euros, Libras, Yenes
 }
-val tasasDeCambioEUR: List<Double> = listOf(1.1867,1.0,0.8713,181.46)
 
-val tasasDeCambioUSD: List<Double> = listOf(1.0,0.8426,0.7342,152.90)
+data class TablaTasas(
+    val tasasDeCambioEUR: List<Double> = listOf(1.1867, 1.0, 0.8713, 181.46),
+    val tasasDeCambioUSD: List<Double> = listOf(1.0, 0.8426, 0.7342, 152.90),
+    val tasasDeCambioGBP: List<Double> = listOf(1.3620, 1.1478, 1.0, 208.23),
+    val tasasDeCambioJPY: List<Double> = listOf(0.654022, 0.55118, 0.480227, 1.0)
+)
 
-val tasasDeCambioGBP: List<Double> = listOf(1.3620,1.1478,1.0,208.23)
-
-val tasasDeCambioJPY: List<Double> = listOf(0.654022,0.55118,0.480227,1.0)
-
-fun actualizarTasas(tasasDeCambio: List<Double>, nuevaTasa: Double, divisaActualizarTasa: Divisas): List<Double>
-{
-    val nuevaListaDeTasas = tasasDeCambio.mapIndexed { index, tasa ->
-        if (index == divisaActualizarTasa.ordinal) nuevaTasa
-        else tasa
+fun actualizarTasas(tasasDeCambio: TablaTasas, nuevaTasa: Double, tablaActualizar: Divisas, divisaActualizarTasa: Divisas): TablaTasas {
+    when (tablaActualizar) {
+        Divisas.Dolares -> return tasasDeCambio.copy(
+            tasasDeCambioUSD= tasasDeCambio.tasasDeCambioUSD.mapIndexed { index, tasa ->
+                if (index == divisaActualizarTasa.ordinal) nuevaTasa
+                else tasa
+            }
+        )
+        Divisas.Euros -> return tasasDeCambio.copy(
+            tasasDeCambioEUR = tasasDeCambio.tasasDeCambioEUR.mapIndexed { index, tasa ->
+                if (index == divisaActualizarTasa.ordinal) nuevaTasa
+                else tasa
+            }
+        )
+        Divisas.Libras -> return tasasDeCambio.copy(
+            tasasDeCambioGBP = tasasDeCambio.tasasDeCambioGBP.mapIndexed { index, tasa ->
+                if (index == divisaActualizarTasa.ordinal) nuevaTasa
+                else tasa
+            }
+        )
+        else -> return tasasDeCambio.copy(
+            tasasDeCambioJPY = tasasDeCambio.tasasDeCambioJPY.mapIndexed { index, tasa ->
+                if (index == divisaActualizarTasa.ordinal) nuevaTasa
+                else tasa
+            }
+        )
     }
-    return nuevaListaDeTasas
 }
 
